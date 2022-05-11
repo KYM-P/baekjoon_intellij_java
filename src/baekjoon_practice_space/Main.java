54e80ae276f10c9ac5ba71e199334c310ead6672
KYM-P <91421805+KYM-P@users.noreply.github.com>
2022-05-11 23:45:50 +0900

    commit

--- src/baekjoon_practice_space/Main.java
+++ src/baekjoon_practice_space/Main.java
@@ -7,37 +7,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         // list input
-        int N = sc.nextInt();
-        int div = 9901;
+        String N = sc.nextLine();
 
         // start
-        int[][] DP = new int[N+1][3];
-        DP[0][0] = 1;
-        for (int i = 1; i <= N; i++) {
-            DP[i][0] = (DP[i-1][0] + DP[i-1][1] + DP[i-1][2])%div;
-            DP[i][1] = (DP[i-1][0] + DP[i-1][2])%div;
-            DP[i][2] = (DP[i-1][0] + DP[i-1][1])%div;
-        }
-        int result = (DP[N][0] + DP[N][1] + DP[N][2])%div;
-
-        System.out.println(result);
-        System.out.println(other(N,div));
-    }
-
-    public static int other (int N, int div) {
-        int[] DP = new int[N+1];
+        int[] DP = new int[N.length()];
         DP[0] = 1;
-        DP[1] = 3;
-        for (int i = 2; i <= N; i++) {
+        int div = 1000000;
+
+        for (int i = 1; i < N.length(); i++) {
             DP[i] += (DP[i-1])%div;
-            DP[i] += (DP[i-2] * 2)%div;
-            if (DP[i-1] - DP[i-2] < 0){
-                DP[i] += (DP[i-1] - DP[i-2] + div)%div;
-            }
-            else {
-                DP[i] += (DP[i-1] - DP[i-2])%div;
+            if (((int)N.charAt(i) - '0') + ((int)N.charAt(i-1) - '0') * 10 < 27) {
+                if (i == 1) {
+                    DP[i] += 1;
+                }
+                else {
+                    DP[i] += (DP[i-2])%div;
+                }
             }
         }
-        return DP[N] % div;
+        System.out.println(DP[N.length()-1]%div);
     }
 }
