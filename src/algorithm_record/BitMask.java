2cba3c9a7b6525f41c9e9aaa15647b7bd7be483d
KYM-P <91421805+KYM-P@users.noreply.github.com>
2022-05-17 23:57:18 +0900

    commit

---
+++ src/algorithm_record/BitMask.java
@@ -0,0 +1,10 @@
+package algorithm_record;
+
+public class BitMask {
+    public static void main(String[] args) {
+        int Bitm = 0; // 0000
+        int N = 3;
+        Bitm = 1 << N; // 1000 =  8
+        System.out.println(Bitm);
+    }
+}
--- src/baekjoon_practice_space/Main.java
+++ src/baekjoon_practice_space/Main.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class Main {
     static int N;
     static int[][] map;
-    static boolean[] visited;
-    public static void main(String[] args) { // dp   TSP(TSProblem)
+    static int bitm = 0;
+    static int mini = 0;
+    static Integer[][] DP;
+    public static void main(String[] args) { // TSP(TSProblem) dp + BitMask
         Scanner sc = new Scanner(System.in);
 
         // list input
         N = sc.nextInt();
         map = new int[N][N];
-        visited = new boolean[N];
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -21,5 +22,18 @@ public class Main {
         }
 
         // start
+
+        DP = new Integer[(1<<N)-1][N]; // DP[bitmask][N]
+        bitm = 1;
+    }
+    public static void root(int start, int bm) {
+        for (int i = 1; i < N; i++) {
+            if ((bm & (1 << i)) == 0 && map[start][i] != 0) { // i 번째 Node 통과 여부
+                bitm = (bitm | (1 << i));
+                mini += map[start][i];
+                root(i,bitm);
+
+            }
+        }
     }
 }
