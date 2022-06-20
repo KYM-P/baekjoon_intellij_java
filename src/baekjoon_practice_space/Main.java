c7bfcdc7d0acf41a6e94edc254bec5730e811bd7
KYM-P <91421805+KYM-P@users.noreply.github.com>
2022-06-21 00:06:50 +0900

    commit

--- src/baekjoon_practice_space/Main.java
+++ src/baekjoon_practice_space/Main.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 
 public class Main {
     static int N;
+    static String table;
+    static String goal;
     static int[][] DP;
     public static void main(String[] args) { // dp
         Scanner sc = new Scanner(System.in);
 
         // list input
         N = sc.nextInt();
-
+        table = sc.nextLine();
+        goal = sc.nextLine();
         // start
-
     }
 }
