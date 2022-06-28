7f9c8869d9b72b8909cb0c3845b122b679485290
KYM-P <91421805+KYM-P@users.noreply.github.com>
2022-06-28 23:55:27 +0900

    commit

--- src/baekjoon_practice_space/Main.java
+++ src/baekjoon_practice_space/Main.java
@@ -25,15 +25,54 @@ public class Main {
         Down = new int[N + 1];
         DP = new int[N + 1];
         //System.out.println(table + " " + goal);
-        for(int i = 0; i < 10; i ++) {
+        for (int i = 1; i <= N; i++) {
 
+            if (i > 1) {
+
+            }
+            if (i > 2) {
+
+            }
         }
 
     }
-    public static void second (int a, int b) {
+    public static int up_size(){
 
     }
-    public static void third (int a, int b, int c) {
+    public static int first (int a, int lastindex) {
+        int count = 0;
+        return count;
+    }
+    public static int second (int a, int b, int lastindex) {
+        int count = 0;
 
+        return count;
+    }
+    public static int third (int a, int b, int c, int lastindex) {
+        int count = 0;
+        int up = 0;
+        int down = 0;
+        if (a == b && b == c) {
+            count += a/3 + ((a%3 > 0)?1:0);
+            return count;
+
+        }
+        else if (a == b) {
+            count += a/3 + ((a%3 > 0)?1:0);
+            return count + Math.min(first(table.charAt(lastindex), lastindex), first(c, lastindex));
+        }
+        else if (b == c) {
+            count += b/3 + ((b%3 > 0)?1:0);
+            return count + + first(a, lastindex - 2);
+        }
+        else if (c == a) {
+            count += c/3 + ((c%3 > 0)?1:0);
+            return count + + first(b, lastindex - 1);
+        }
+        else {
+            count += a/3 + ((a%3 > 0)?1:0);
+        }
+
+        return count;
     }
 }
