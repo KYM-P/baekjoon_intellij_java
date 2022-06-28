d3121e7b199d62a68c9acc83f9ffe33c6ed44481
KYM-P <91421805+KYM-P@users.noreply.github.com>
2022-06-29 01:32:27 +0900

    commit

--- src/baekjoon_practice_space/Main.java
+++ src/baekjoon_practice_space/Main.java
@@ -36,11 +36,31 @@ public class Main {
         }
 
     }
-    public static int up_size(){
-
+    public static int up_size(int table_n,int goal_n){
+        if (table_n > goal_n) {
+            return (10 - table_n) + goal_n;
+        }
+        else if (table_n < goal_n) {
+            return goal_n - table_n;
+        }
+        else { // table-N == goal_n
+            return  0;
+        }
     }
-    public static int first (int a, int lastindex) {
+    public static int down_size(int table_n,int goal_n){
+        if (table_n > goal_n) {
+            return goal_n - table_n;
+        }
+        else if (table_n < goal_n) {
+            return (10 - table_n) + goal_n;
+        }
+        else { // table-N == goal_n
+            return  0;
+        }
+    }
+    public static int first (int a) {
         int count = 0;
+        count += a/3 + ((a%3 > 0)?1:0);
         return count;
     }
     public static int second (int a, int b, int lastindex) {
@@ -48,10 +68,12 @@ public class Main {
 
         return count;
     }
-    public static int third (int a, int b, int c, int lastindex) {
+    public static int third (int a, int b, int c, int lastindex, boolean up_t) {
         int count = 0;
         int up = 0;
         int down = 0;
+        int main = 0;
+        int m_index = 0;
         if (a == b && b == c) {
             count += a/3 + ((a%3 > 0)?1:0);
             return count;
@@ -59,20 +81,32 @@ public class Main {
         }
         else if (a == b) {
             count += a/3 + ((a%3 > 0)?1:0);
-            return count + Math.min(first(table.charAt(lastindex), lastindex), first(c, lastindex));
+            main = a;
+            m_index = 0;
         }
         else if (b == c) {
             count += b/3 + ((b%3 > 0)?1:0);
-            return count + + first(a, lastindex - 2);
+            main = b;
+            m_index = 1;
         }
         else if (c == a) {
             count += c/3 + ((c%3 > 0)?1:0);
-            return count + + first(b, lastindex - 1);
+            main = c;
+            m_index = 2;
         }
         else {
             count += a/3 + ((a%3 > 0)?1:0);
         }
-
-        return count;
+        // if ~ else if
+        if (up_t) {
+            up = (table.charAt(lastindex - m_index) - '0') + main;
+            up = (up>9)?up-10:up;
+            return count + Math.min(first(up_size(up ,goal.charAt(lastindex - m_index) - '0')), first(down_size(up ,goal.charAt(lastindex - m_index) - '0')));
+        }
+        else {
+            down = (table.charAt(lastindex - m_index) - '0') - main;
+            down = (down<0)?down+10:down;
+            return count + Math.min(first(up_size(down ,goal.charAt(lastindex - m_index) - '0')), first(down_size(down ,goal.charAt(lastindex - m_index) - '0')));
+        }
     }
 }
