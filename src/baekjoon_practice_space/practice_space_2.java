1b7d4774cd57bd0ed4f5b38e098f95dd7c36c037
KYM-P <91421805+KYM-P@users.noreply.github.com>
2022-06-29 23:25:45 +0900

    commit

--- src/baekjoon_practice_space/practice_space_2.java
+++ src/baekjoon_practice_space/practice_space_2.java
@@ -7,92 +7,99 @@ import java.util.*;
 
 public class practice_space_2 {
 
-    private static int dp[][];
+    static int N;
+    static String table;
+    static String goal;
+    static int[][] UP;
+    static int[][] Down;
+    static int[] DP;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) { // dp
+        Scanner sc = new Scanner(System.in);
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        // list input
+        N = sc.nextInt();
+        sc.nextLine();
+        table = sc.nextLine();
+        goal = sc.nextLine();
 
-        StringTokenizer st =new StringTokenizer(bf.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        // DP[M][N] = M번 도시 방문 했을 때 도착 도시 번호가 N
-        dp = new int[M+1][N+1];
-
-        List<Node> boards[] = new List[N+1];
-
-        for(int i=0;i<=N;i++){
-            boards[i]=new ArrayList<>();
-        }
-
-        for(int i=0;i<K;i++){
-
-            st = new StringTokenizer(bf.readLine());
-
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            if(a > b){
-                continue;
-            }
-
-            boards[a].add(new Node(b,c));
-        }
-
-        int result=0;
-
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(1);
-
-        int cnt=1;
-
-        while(!q.isEmpty() && cnt < M){
-
-            int size = q.size();
-
-            while(size-- > 0){
-                int nowIndex = q.poll();
-                for(Node nextNode : boards[nowIndex]){
-
-                    int nextIndex = nextNode.index;
-                    int nextnext = dp[cnt][nowIndex]+nextNode.next;
-
-                    if(dp[cnt+1][nextIndex] >= nextnext){
-                        continue;
-                    }
-
-                    dp[cnt+1][nextIndex] = nextnext;
-
-                    q.add(nextIndex);
+        // start
+        UP = new int[N + 1][10];
+        Down = new int[N + 1][10];
+        DP = new int[N + 1];
+        //System.out.println(table + " " + goal);
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j > goal.charAt(i-1)) {
 
                 }
+                else if (j < goal.charAt(i-1)) {
+
+                }
+                else { // table-N == goal_n
+                    UP[i][j] = 0;
+                    Down[i][j] = 0;
+                }
+            }
+            if (i > 1) {
 
             }
-            cnt++;
+            if (i > 2) {
 
+            }
         }
-
-        for(int i=2;i<=M;i++){
-            result = Integer.max(result,dp[i][N]);
-        }
-
-        System.out.println(result);
-
 
     }
 
-    public static class Node{
-        int index;
-        int next;
+    public static int first (int a) {
+        int count = 0;
+        count += a/3 + ((a%3 > 0)?1:0);
+        return count;
+    }
+    public static int second (int a, int b, int lastindex) {
+        int count = 0;
 
-        public Node(int index, int next) {
-            this.index = index;
-            this.next = next;
+        return count;
+    }
+    public static int third (int a, int b, int c, int lastindex, boolean up_t) {
+        int count = 0;
+        int up = 0;
+        int down = 0;
+        int main = 0;
+        int m_index = 0;
+        if (a == b && b == c) {
+            count += a/3 + ((a%3 > 0)?1:0);
+            return count;
+
+        }
+        else if (a == b) {
+            count += a/3 + ((a%3 > 0)?1:0);
+            main = a;
+            m_index = 0;
+        }
+        else if (b == c) {
+            count += b/3 + ((b%3 > 0)?1:0);
+            main = b;
+            m_index = 1;
+        }
+        else if (c == a) {
+            count += c/3 + ((c%3 > 0)?1:0);
+            main = c;
+            m_index = 2;
+        }
+        else {
+            count += a/3 + ((a%3 > 0)?1:0);
+        }
+        // if ~ else if
+        if (up_t) {
+            up = (table.charAt(lastindex - m_index) - '0') + main;
+            up = (up>9)?up-10:up;
+            return count + Math.min(first(up_size(up ,goal.charAt(lastindex - m_index) - '0')), first(down_size(up ,goal.charAt(lastindex - m_index) - '0')));
+        }
+        else {
+            down = (table.charAt(lastindex - m_index) - '0') - main;
+            down = (down<0)?down+10:down;
+            return count + Math.min(first(up_size(down ,goal.charAt(lastindex - m_index) - '0')), first(down_size(down ,goal.charAt(lastindex - m_index) - '0')));
         }
     }
 }
