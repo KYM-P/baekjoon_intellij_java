9050026abd891fdad24f8f72f8cb7d7a875ecff7
KYM-P <k_ym02@naver.com>
2024-01-09 23:30:22 +0900

    20240109
    20240109

--- src/baekjoon_practice_since2024/Main.java
+++ src/baekjoon_practice_since2024/Main.java
@@ -3,40 +3,116 @@ package baekjoon_practice_since2024;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main
 {
-    static int N;
+    static int R;
+    static int C;
+    static int startx;
+    static int starty;
+    static char[][] table;
+    static boolean[][] visit;
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        PriorityQueue<Integer> Up = new PriorityQueue<>();
-        PriorityQueue<Integer> Down = new PriorityQueue<>(Collections.reverseOrder());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            int num =Integer.parseInt(br.readLine());
-            if (Up.size() == Down.size()) {
-                Down.offer(num);
-            }
-            else {
-                Up.offer(num);
-            }
-            if (!Up.isEmpty() && !Down.isEmpty()){
-                if (Up.peek() < Down.peek()) {
-                    int C = Down.poll();
-                    Down.offer(Up.poll());
-                    Up.offer(C);
+        table = new char[C][R];
+        visit = new boolean[C][R];
+        for (int i = 0; i < R; i++) {
+            sb.append(br.readLine());
+            for (int j = 0; j < C; j++) {
+                table[j][i] = sb.charAt(j);
+                if (table[j][i] == 'L'){
+                    startx = j;
+                    starty = i;
                 }
             }
-            //System.out.print(Up);
-            //System.out.println(Down);
-            sb.append(Integer.toString(Down.peek())+"\n");
+            sb.delete(0,C);
         }
-        System.out.println(sb);
+        int count = 0;
+        while (!DFS(startx, starty)) {
+            patch();
+            count++;
+            //System.out.println(count);
+        }
+        /*
+        for (int y = 0; y < R; y++) {
+            for (int x = 0; x < C; x++) {
+                System.out.print(table[x][y]);
+            }
+            System.out.println();
+        }
+
+         */
+        System.out.println(count);
+    }
+    public static boolean DFS(int x, int y) {
+        visit[x][y] = true;
+        //System.out.println(x + " " + y + " " + startx + " " + starty + " " + (table[x][y] == 'L'));
+        if (table[x][y] == 'L' && !(x == startx && y == starty)) {
+            return true;
+        }
+        if (x+1 < C && table[x+1][y] != 'X' && visit[x+1][y] == false) {
+            if (DFS(x+1,y)) {
+                return true;
+            }
+        }
+        if (x-1 >= 0 && table[x-1][y] != 'X' && visit[x-1][y] == false) {
+            if (DFS(x-1,y)) {
+                return true;
+            }
+        }
+        if (y-1 >= 0 && table[x][y-1] != 'X' && visit[x][y-1] == false) {
+            if (DFS(x,y-1)) {
+                return true;
+            }
+        }
+        if (y+1 < R && table[x][y+1] != 'X' && visit[x][y+1] == false) {
+            if (DFS(x,y+1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void patch() {
+        Queue<Integer> q_x = new LinkedList<Integer>();
+        Queue<Integer> q_y = new LinkedList<Integer>();
+        for (int y = 0; y < R; y++) {
+            for (int x = 0; x < C; x++) {
+                visit[x][y] = false;
+                if (table[x][y] == 'X') {
+                    if (x+1 < C && table[x+1][y] == '.') {
+                        q_x.offer(x);
+                        q_y.offer(y);
+                        table[x][y] = '#';
+                    }
+                    else if (x-1 >= 0 && table[x-1][y] == '.') {
+                        q_x.offer(x);
+                        q_y.offer(y);
+                        table[x][y] = '#';
+                    }
+                    else if (y+1 < R && table[x][y+1] == '.') {
+                        q_x.offer(x);
+                        q_y.offer(y);
+                        table[x][y] = '#';
+                    }
+                    else if (y-1 >= 0 && table[x][y-1] == '.') {
+                        q_x.offer(x);
+                        q_y.offer(y);
+                        table[x][y] = '#';
+                    }
+                }
+            }
+        }
+        while(!q_x.isEmpty()) {
+            table[q_x.poll()][q_y.poll()] = '.';
+        }
     }
 }
