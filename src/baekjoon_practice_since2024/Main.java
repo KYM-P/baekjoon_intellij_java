63219824bc197319ef8f3ae155f2556e3d207c4b
KYM-P <k_ym02@naver.com>
2024-01-12 21:36:34 +0900

    20240112
    20240112

--- src/baekjoon_practice_since2024/Main.java
+++ src/baekjoon_practice_since2024/Main.java
@@ -11,10 +11,13 @@ public class Main
 {
     static int R;
     static int C;
-    static int startx;
-    static int starty;
     static char[][] table;
     static boolean[][] visit;
+    static Queue<int[]> swan;
+    static Queue<int[]> water;
+    static int[] endpoint;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -25,19 +28,30 @@ public class Main
 
         table = new char[C][R];
         visit = new boolean[C][R];
+        swan = new LinkedList<>();
+        water = new LinkedList<>();
+        endpoint = new int[2];
         for (int i = 0; i < R; i++) {
             sb.append(br.readLine());
             for (int j = 0; j < C; j++) {
                 table[j][i] = sb.charAt(j);
                 if (table[j][i] == 'L'){
-                    startx = j;
-                    starty = i;
+                    if (swan.isEmpty()) {
+                        swan.offer(new int[] {j,i});
+                    }
+                    else {
+                        endpoint[0] = j;
+                        endpoint[1] = i;
+                    }
+                }
+                if (table[j][i] == '.') {
+                    water.offer(new int[] {j, i});
                 }
             }
             sb.delete(0,C);
         }
         int count = 0;
-        while (!DFS(startx, starty)) {
+        while (!BFS()) {
             patch();
             count++;
             //System.out.println(count);
@@ -53,32 +67,26 @@ public class Main
          */
         System.out.println(count);
     }
-    public static boolean DFS(int x, int y) {
-        visit[x][y] = true;
-        //System.out.println(x + " " + y + " " + startx + " " + starty + " " + (table[x][y] == 'L'));
-        if (table[x][y] == 'L' && !(x == startx && y == starty)) {
-            return true;
-        }
-        if (x+1 < C && table[x+1][y] != 'X' && visit[x+1][y] == false) {
-            if (DFS(x+1,y)) {
-                return true;
+    public static boolean BFS() {
+        Queue<int []> search = new LinkedList<>();
+        while(swan.isEmpty()) {
+            int[] start = swan.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = start[0] + dx[i];
+                int ny = start[1] + dy[i];
+                if (nx < 0 || ny < 0 || nx > C-1 || ny > R-1 || visit[nx][ny]) {
+                    continue;
+                }
+                visit[nx][ny] = true;
+                if (table[nx][ny] == 'X') {
+                    search.offer(new int[] {nx, ny});
+                }
+                else if (table[nx][ny] == '.') {
+                    swan.offer(new int[] {nx,ny});
+                }
             }
         }
-        if (x-1 >= 0 && table[x-1][y] != 'X' && visit[x-1][y] == false) {
-            if (DFS(x-1,y)) {
-                return true;
-            }
-        }
-        if (y-1 >= 0 && table[x][y-1] != 'X' && visit[x][y-1] == false) {
-            if (DFS(x,y-1)) {
-                return true;
-            }
-        }
-        if (y+1 < R && table[x][y+1] != 'X' && visit[x][y+1] == false) {
-            if (DFS(x,y+1)) {
-                return true;
-            }
-        }
+        swan = search;
         return false;
     }
     public static void patch() {
